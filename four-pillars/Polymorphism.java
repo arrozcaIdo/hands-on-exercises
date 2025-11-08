a8d94f3349a9904263ab7f7fec3cc39885c807ec
Elyssa Marie Manango <142665230+arrozcaIdo@users.noreply.github.com>
2025-11-08 17:27:15 +0800

    Delete four-pillars/Polymorphism.java

--- four-pillars/Polymorphism.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-
-public class Polymorphism {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("test.");
-    }
-}
