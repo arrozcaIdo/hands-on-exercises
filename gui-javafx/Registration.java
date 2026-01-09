632ff1af82f42b9f6cb3520d5a5a55c84dfaa750
Elyssa Marie Manango <142665230+arrozcaIdo@users.noreply.github.com>
2026-01-10 00:13:04 +0800

    added existing acc confirmation

--- gui-javafx/Registration.java
+++ gui-javafx/Registration.java
@@ -1,5 +1,3 @@
-// email and name registration form n sobrang simple lng cuz dumudugo na utak q
-
 package tries;
 
 import javax.swing.*;
@@ -9,13 +7,13 @@ public class Registration {
     
     JFrame frame = new JFrame("Registration Form (SIMPLE)");
     JPanel panel = new JPanel();
-    
+   
     Registration(){
-        frame.setSize(400, 250);  
+        frame.setSize(400, 250); 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
-        panel.setLayout(new GridLayout(4, 2, 10, 10));
-        panel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+        panel.setLayout(new GridLayout(4, 2, 10, 10)); 
+        panel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20)); 
         
         panel.add(new JLabel("Name:"));
         JTextField nameField = new JTextField();
@@ -26,12 +24,19 @@ public class Registration {
         panel.add(emailField);
         
         panel.add(new JLabel(""));
-        panel.add(new JLabel(""));
         
         JButton submitButton = new JButton("Submit");
         panel.add(submitButton);
         
         frame.add(panel);
+        
+        int confirmation = JOptionPane.showOptionDialog(null, "Already have an account?", "Registration",JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, submitButton);
+        if (confirmation == 0) {
+        	frame.setVisible(true);
+        } else {
+        	JOptionPane.showMessageDialog(null, "Go create an account first", "YOU SHALL NOT PASS!!!", 0);
+        }
+        
         
         submitButton.addActionListener(e -> {
             String name = nameField.getText().trim();
@@ -45,7 +50,6 @@ public class Registration {
             }
         });
         
-        frame.setVisible(true);
     }
     
     public static void main(String[] args) {
